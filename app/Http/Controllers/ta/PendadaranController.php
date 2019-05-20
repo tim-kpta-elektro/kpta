3201ce5e9a218351a811da1cdad81f8f2f676440
nitryuu <kscdh6@gmail.com>
2019-05-20 14:03:55 +0700

    message

---
+++ app/Http/Controllers/ta/PendadaranController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\ta;
+use App\Http\Controllers\Controller;
+use PDF;
+
+class PendadaranController extends Controller
+{
+    public function cetak_persetujuan(){
+
+        $pdf = PDF::loadview('/ta/laporan/cetak_persetujuan');
+        return $pdf->stream();
+    }
+
+    public function cetak_undangan(){
+    	$pdf = PDF::loadview('/ta/laporan/cetak_undangan');
+    	return $pdf->stream();
+    }
+
+    public function cetak_bukti(){
+    	$pdf = PDF::loadview('/ta/laporan/cetak_bukti');
+    	return $pdf->stream();
+    }
+}
+
+?>
---
+++ resources/views/kp/cetak_form.blade.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Konsultasi KP</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style type="text/css">
+        
+        table.table1 * {
+            border: none;
+            line-height: 1.2;
+        }
+
+        .table1 td {
+            vertical-align: text-top;
+            text-align: left;
+        }
+
+        table.table2 {
+          border-collapse: collapse;
+          width: 100%;
+        }
+
+        .table2 table.table2, th, td {
+          border: 1px solid black;
+        }
+
+        .table2 th, td {
+          text-align: center;
+          padding: 8px;
+        }
+        .table2 td {
+            height: 25px;
+        }
+
+        .table2 tr:nth-child(even){background-color: #f2f2f2}
+
+        .table2 th {
+          background-color: #3c73a3;
+          color: white;
+        }
+
+        .page-break {
+            page-break-after: always;
+        }
+
+        footer {
+          position: fixed; 
+          bottom: 0cm; 
+          left: 2.5cm; 
+          right: 0cm;
+          height: 2cm;
+
+          /** Extra personal styles **/
+          /*background-color: #03a9f4;*/
+          text-align: justify;
+          font-size: 9pt;
+          line-height: 1;
+          color: black;
+          text-align: left;
+        }
+
+    </style>
+</head>
+<body>
+    <footer>
+     <p><strong>Catatan :</strong><br>
+        1. Lembar pantauan ditandatangani dosen pembimbing selama penyusunan proposal & laporan akhir.<br>
+        2. Lembar konsultasi ditanda tangani pembimbing lapangan dan distempel selama kegiatan di lapangan.</p>
+        <span style="background-color: #000000; color: white;">
+            TE-KP-002
+        </span>
+    </footer>
+
+    <div class="container">
+        <div class="row">
+           <div class="col">
+               <h2 style="text-align: center;">Lembar Konsultasi Kerja Praktek</h2>
+                <table class="table1" style="width: 100%">    
+                    <tr>
+                        <td style="width: 35%;">Nama Mahasiswa</td>
+                        <td style="width: 3%;">:</td>
+                        <td style="width: 62%;"><strong>Yudhi Prabowo Kusuma</strong></td>
+                    </tr>
+                    <tr>
+                        <td>NIM</td>
+                        <td>:</td>
+                        <td><strong>I0716034</strong></td>
+                    </tr>
+                    <tr>
+                        <td>Dosen Pembimbing</td>
+                        <td>:</td>
+                        <td><strong>Meiyanto Eko Sulistyo, S.T., M.Eng / 197705132009121004</strong></td>
+                    </tr>
+                    <tr>
+                        <td>Pembimbing Lapangan</td>
+                        <td>:</td>
+                        <td style="vertical-align: text-center;">______________________</td>
+                    </tr>
+                    <tr>
+                        <td>Tempat Kerja Praktek (KP)</td>
+                        <td>:</td>
+                        <td><strong>PT. Telkomsel</strong></td>
+                    </tr>
+                    <tr>
+                        <td>Alamat Tempat KP</td>
+                        <td>:</td>
+                        <td><strong>Jl.Gatot Subroto No.Kav.52, RT.6/RW.1, Kuningan Bar., Mampang Prpt., Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12710</strong></td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal Kerja Praktek (KP)</td>
+                        <td>:</td>
+                        <td><strong>sd.</strong></td>
+                    </tr>
+                </table>
+                <br>
+                <table class="table2">
+                  <tr>
+                    <th style="width: 5%;">No</th>
+                    <th style="width: 20%;" >Tanggal</th>
+                    <th style="width: 65%;" >Uraian Kegiatan</th>
+                    <th style="width: 10%;">Paraf Pembb.</th>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                </table>      
+           </div>     
+        </div>
+    </div>
+    <div class="page-break"></div>
+    <div class="container">
+        <div class="row">
+           <div class="col">
+               <table class="table2">
+                  <tr>
+                    <th style="width: 5%;">No</th>
+                    <th style="width: 20%;" >Tanggal</th>
+                    <th style="width: 65%;" >Uraian Kegiatan</th>
+                    <th style="width: 10%;">Paraf Pembb.</th>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                </table>      
+           </div>     
+        </div>
+    </div>
+</body>
+</html>
