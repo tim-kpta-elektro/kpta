bfbc72ce3f30a35db5b5712263a1a56caf864f9f
unknown <yudhie123@gmail.com>
2019-05-23 13:59:50 +0700

    Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('_partials/master')
 @section('konten')
                 <!-- Hero -->
-                <div class="bg-image overflow-hidden" style="background-image: url('/kpta/assets/media/photos/photo3@2x.jpg');">
+                <div class="bg-image overflow-hidden" style="background-image: url('/kpta/assets/media/photos/photo34@2x.jpg');">
                     <div class="bg-primary-dark-op">
                         <div class="content content-narrow content-full">
                             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center mt-5 mb-2 text-center text-sm-left">
@@ -22,32 +22,32 @@
                         <div class="col-6 col-md-3 col-lg-6 col-xl-3">
                             <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
                                 <div class="block-content block-content-full">
-                                    <div class="font-size-sm font-w600 text-uppercase text-muted">Visitors</div>
-                                    <div class="font-size-h2 font-w400 text-dark">120,580</div>
+                                    <div class="font-size-sm font-w600 text-uppercase text-muted">Pengunjung</div>
+                                    <div class="font-size-h2 font-w400 text-dark">120</div>
                                 </div>
                             </a>
                         </div>
                         <div class="col-6 col-md-3 col-lg-6 col-xl-3">
                             <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
                                 <div class="block-content block-content-full">
-                                    <div class="font-size-sm font-w600 text-uppercase text-muted">Sales</div>
-                                    <div class="font-size-h2 font-w400 text-dark">150</div>
+                                    <div class="font-size-sm font-w600 text-uppercase text-muted">Total Mahasiswa</div>
+                                    <div class="font-size-h2 font-w400 text-dark">350</div>
                                 </div>
                             </a>
                         </div>
                         <div class="col-6 col-md-3 col-lg-6 col-xl-3">
                             <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
                                 <div class="block-content block-content-full">
-                                    <div class="font-size-sm font-w600 text-uppercase text-muted">Earnings</div>
-                                    <div class="font-size-h2 font-w400 text-dark">$3,200</div>
+                                    <div class="font-size-sm font-w600 text-uppercase text-muted">Mahasiswa Aktif</div>
+                                    <div class="font-size-h2 font-w400 text-dark">200</div>
                                 </div>
                             </a>
                         </div>
                         <div class="col-6 col-md-3 col-lg-6 col-xl-3">
                             <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
                                 <div class="block-content block-content-full">
-                                    <div class="font-size-sm font-w600 text-uppercase text-muted">Avg Sale</div>
-                                    <div class="font-size-h2 font-w400 text-dark">$21</div>
+                                    <div class="font-size-sm font-w600 text-uppercase text-muted">Alumni</div>
+                                    <div class="font-size-h2 font-w400 text-dark">50</div>
                                 </div>
                             </a>
                         </div>
