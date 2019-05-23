28e14edd81958f77482a48aaabea8c9142014953
adip240499 <adip240499@gmail.com>
2019-05-23 14:53:28 +0700

    Side Bar Pengajuan TA

--- resources/views/_partials/master.blade.php
+++ resources/views/_partials/master.blade.php
@@ -208,6 +208,12 @@
                         </li>
                         <li class="nav-main-heading">Tugas Akhir</li>
                         <li class="nav-main-item">
+                            <a class="nav-main-link" aria-haspopup="true" aria-expanded="false" href="/kpta/public/ta/pengajuan">
+                                <i class="nav-main-link-icon si si-book-open"></i>
+                                <span class="nav-main-link-name">Pengajuan TA</span>
+                            </a>
+                        </li>
+                        <li class="nav-main-item">
                             <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
                                 <i class="nav-main-link-icon si si-wrench"></i>
                                 <span class="nav-main-link-name">Pengajuan Tugas Akhir</span>
