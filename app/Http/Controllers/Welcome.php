bf75328ea990fd8b70141f5d4ebfa1bb0388743f
unknown <yudhie123@gmail.com>
2019-06-13 01:42:48 +0700

    Revisi

--- app/Http/Controllers/Welcome.php
+++ app/Http/Controllers/Welcome.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class Welcome extends Controller
 {
     //
+    public function __construct()
+    {
+    	$this->middleware(function ($request, $next) {
+	    	if(Session::get('login')){
+	            return redirect('dashboard');
+	        }else{
+	            return $next($request);
+	        }
+    	});
+    }
+
     public function index(){
     	$listkp = DB::table('daftar_kp')
 	    			->select('nim','mhs_nama','perusahaan_nama','perusahaan_almt')
