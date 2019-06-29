e4603bc137645e6345383dd8946b3ffbcf765a2b
unknown <yudhie123@gmail.com>
2019-06-29 16:40:08 +0700

    fix route

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -15,7 +15,7 @@ class Admin extends Controller
         $this->middleware(function ($request, $next) {
 	        if(!Session::get('login')){
 	            return redirect('login')->with('alert','Kamu harus login dulu');
-	        }elseif (Session::get('level') !== 2) {
+	        }elseif (!Session::get('level') == 2) {
 	        	return redirect('dashboard');
 	        }else{
 	            return $next($request);
